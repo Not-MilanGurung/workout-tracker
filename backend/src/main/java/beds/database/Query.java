97a2d488809434d25ca3bc834d8d85a114ab6c31
Not-MilanGurung <notmilangurung@gmail.com>
2025-03-05 13:39:07 +0545

    Testing the sucessful execution of sample table creation and initialisaion

---
+++ backend/src/main/java/beds/database/Query.java
@@ -0,0 +1,26 @@
+package beds.database;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Query {
+    public static void main(String[] args) {
+        try {
+            DatabaseConnection.main(args);
+            Connection con = DatabaseConnection.con;
+            Statement stmt = con.createStatement();
+
+            ResultSet result = stmt.executeQuery("Select * from Exercises");
+            while (result.next()){
+                System.out.printf("%d %s %d %d %d %d %d %s\n",result.getInt(1), result.getString(2),
+                result.getInt(3),result.getInt(4),result.getInt(5),result.getInt(6),result.getInt(7),result.getTime(8));
+            }
+        }
+        catch (SQLException e){
+            e.printStackTrace(System.out);
+        }
+
+    }
+}

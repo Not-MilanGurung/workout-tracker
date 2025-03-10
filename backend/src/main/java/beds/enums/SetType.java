4b79fef50206131ddaab4db43b39c7c45e4ffe42
Not-MilanGurung <notmilangurung@gmail.com>
2025-03-10 11:20:24 +0545

    Added table creation for workout, workout_exercise, set and set type

--- backend/src/main/java/beds/enums/SetType.java
+++ backend/src/main/java/beds/enums/SetType.java
@@ -1,5 +1,8 @@
 package beds.enums;
 
 public enum SetType {
-    NORMAL, FALIURE, SUPERSET, DROPSET
+    NORMAL(1), FALIURE(2), DROPSET(3);
+	private int id;
+	private SetType(int id) {this.id = id;}
+	public int getID() {return id;}
 }
